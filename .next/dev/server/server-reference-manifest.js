db19eaf59afba699b37c5d5cb9df3f2b465826d6
Facer Tech <facertechh@gmail.com>
2026-07-07 14:26:16 +0300

    işleyiş

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"5aCYI2CJzs/1wkF0ixBWc/975Ko9RU+Bcuo0l3mAHkM=\"\n}"
--- .next/dev/static/chunks/src_app_page_tsx_002ye8~._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__0~anh68._.js","static/chunks/node_modules_next_0ulkz5h._.js","static/chunks/node_modules_motion-dom_dist_es_0n_wqqr._.js","static/chunks/node_modules_framer-motion_dist_es_129u8xh._.js","static/chunks/node_modules_@supabase_postgrest-js_dist_index_mjs_0eu3ylr._.js","static/chunks/node_modules_@supabase_auth-js_dist_module_0.pzobh._.js","static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_0ezz.e5._.js","static/chunks/node_modules_0d_q_td._.js","static/chunks/[next]_internal_font_google_09ew~0m._.css"],
-    source: "dynamic"
-});
